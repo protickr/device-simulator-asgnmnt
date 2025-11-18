108917b36468e4f0495db8f34d727efe2f4b8374
Protick Roy <protickrr@gmail.com>
2025-11-19 01:41:35 +0600

    fix(web): complete UI upto fan speed slider

--- web/src/components/FanControl.tsx
+++ web/src/components/FanControl.tsx
@@ -1,9 +1,38 @@
+import { useState } from "react";
 import styles from "./FanControl.module.css";
 
 function FanControl() {
+  const [speed, setSpeed] = useState(0);
+
   return (
     <div className={styles.fanControl}>
-      <h1>Fan controls </h1>
+      <div className={styles.power}>
+        <div>
+          <span className={styles.label}>Power</span>
+        </div>
+        <button aria-label="toggle-power"> power</button>
+      </div>
+
+      <div className={styles.speed}>
+        <div className={styles.speedInfo}>
+          <span className={styles.label}>Speed</span>
+          <div className={styles.speedValue}>
+            <span className={styles.label}>{speed}%</span>
+          </div>
+        </div>
+
+        <input
+          type="range"
+          name="speed"
+          id="speed"
+          min={0}
+          max={100}
+          step={1}
+          className={styles.speedRange}
+          value={speed}
+          onChange={(e) => setSpeed(Number(e.target.value))}
+        />
+      </div>
     </div>
   );
 }
